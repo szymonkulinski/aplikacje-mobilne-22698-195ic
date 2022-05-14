988d5506759c43220b72c3ab22f9dd0654d46c6d
szymonkulinski <56955430+szymonkulinski@users.noreply.github.com>
2022-05-14 18:13:44 +0200

    update

--- Lab4/obsluga-danych-wprowadzonych-przez-uzytkownika/App.js
+++ Lab4/obsluga-danych-wprowadzonych-przez-uzytkownika/App.js
@@ -2,7 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React,{ useState, Suspense } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {ActivityIndicator, SafeAreaView, StyleSheet, Text, TextInput, View, TouchableOpacity, Platform, ScrollView } from 'react-native';
+import {ActivityIndicator, SafeAreaView, StyleSheet, Text, TextInput, View, TouchableOpacity, Platform, ScrollView, Pressable, Modal, Switch, Button, Alert, ToastAndroid  } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 function TextTest({ navigation }) {
   const [text, onChangeText] = React.useState("Text 1");
@@ -10,7 +12,6 @@ function TextTest({ navigation }) {
   const [text3, onChangeText3] = React.useState("Text 3");
   const [number, onChangeNumber] = React.useState(null);
   return (
-    //Array 100 liczb 
     <View>
       <View style={styles.navBar}>
         
@@ -22,16 +23,16 @@ function TextTest({ navigation }) {
         <Text> Select </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Switch" onPress={() => navigation.navigate('Switch')} >
-      <Text> Switch </Text>
+      <TouchableOpacity style={styles.button} title="SwitchPage" onPress={() => navigation.navigate('SwitchPage')} >
+      <Text> SwitchPage </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Date" onPress={() => navigation.navigate('Date')} >
-      <Text> Date </Text>
+      <TouchableOpacity style={styles.button} title="datePage" onPress={() => navigation.navigate('datePage')} >
+      <Text> datePage </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Modal" onPress={() => navigation.navigate('Modal')} >
-      <Text> Modal </Text>
+      <TouchableOpacity style={styles.button} title="ModalPage" onPress={() => navigation.navigate('ModalPage')} >
+      <Text> ModalPage </Text>
       </TouchableOpacity>
       </View>
       <View>
@@ -71,7 +72,7 @@ function TextTest({ navigation }) {
                     style={styles.input}
                     onChangeText={onChangeText3}
                     value={text3}
-                    clearTextOnFocus="true"
+                    clearTextOnFocus={true}
                 />
                 <Text style={styles.text}>
                     Pesel
@@ -82,7 +83,7 @@ function TextTest({ navigation }) {
                     placeholderTextColor='rgb(0, 0, 255)'
                     keyboardType="numeric"
                     autoCompleteType="email"
-                    maxLength="11"
+                    maxLength={11}
                 />
             </SafeAreaView>
           </ScrollView>
@@ -93,6 +94,7 @@ function TextTest({ navigation }) {
 
 
 function Select({ navigation }) {
+  const [selectedValue, setSelectedValue] = useState("prawo");
   return (
     <View>
       <View style={styles.navBar}>
@@ -105,28 +107,63 @@ function Select({ navigation }) {
         <Text> Select </Text>
       </TouchableOpacity>
       
-      <TouchableOpacity style={styles.button} title="Switch" onPress={() => navigation.navigate('Switch')} >
-      <Text> Switch </Text>
+      <TouchableOpacity style={styles.button} title="SwitchPage" onPress={() => navigation.navigate('SwitchPage')} >
+      <Text> SwitchPage </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Date" onPress={() => navigation.navigate('Date')} >
-      <Text> Date </Text>
+      <TouchableOpacity style={styles.button} title="datePage" onPress={() => navigation.navigate('datePage')} >
+      <Text> datePage </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Modal" onPress={() => navigation.navigate('Modal')} >
-      <Text> Modal </Text>
+      <TouchableOpacity style={styles.button} title="ModalPage" onPress={() => navigation.navigate('ModalPage')} >
+      <Text> ModalPage </Text>
       </TouchableOpacity>
       
       </View>
       <View style={styles.content}>
-
+      <Picker
+          selectedValue={selectedValue}
+          style={styles.input}
+          onValueChange={(itemValue, itemIndex)=>setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="Leworęczny" value="lewo"/>
+          <Picker.Item label="Praworęczny" value="prawo"/>
+          <Picker.Item label="Oburęczny" value="obu"/>
+        </Picker>
+        <Picker
+          selectedValue={selectedValue}
+          style={styles.input}
+          onValueChange={(itemValue, itemIndex)=>setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="Leworęczny" value="lewo"/>
+          <Picker.Item label="Praworęczny" value="prawo"/>
+          <Picker.Item label="Oburęczny" value="obu"/>
+        </Picker>
+        <Picker
+          selectedValue={selectedValue}
+          style={styles.input}
+          onValueChange={(itemValue, itemIndex)=>setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="Leworęczny" value="lewo"/>
+          <Picker.Item label="Praworęczny" value="prawo"/>
+          <Picker.Item label="Oburęczny" value="obu"/>
+        </Picker>
       </View>
       
     </View>
   );
 }
 
-function Switch({ navigation }) {
+function SwitchPage({ navigation }) {
+  const [isEnabled, setIsEnabled] = React.useState(false);
+  const [Number, setNumber] = React.useState(1);
+  const toggleSwitch = () => {
+      setIsEnabled(previousState => !previousState);
+      setNumber(Math.floor(Math.random()*(1000)+0))
+      setModalVisible(!isEnabled);
+  };
+  const [modalVisible, setModalVisible] = React.useState(false);
+
   return (
     <View>
       <View style={styles.navBar}>
@@ -139,29 +176,91 @@ function Switch({ navigation }) {
         <Text> Select </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Switch" onPress={() => navigation.navigate('Switch')} >
-      <Text> Switch </Text>
+      <TouchableOpacity style={styles.button} title="SwitchPage" onPress={() => navigation.navigate('SwitchPage')} >
+      <Text> SwitchPage </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Date" onPress={() => navigation.navigate('Date')} >
-      <Text> Date </Text>
+      <TouchableOpacity style={styles.button} title="datePage" onPress={() => navigation.navigate('datePage')} >
+      <Text> datePage </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Modal" onPress={() => navigation.navigate('Modal')} >
-      <Text> Modal </Text>
+      <TouchableOpacity style={styles.button} title="ModalPage" onPress={() => navigation.navigate('ModalPage')} >
+      <Text> ModalPage </Text>
       </TouchableOpacity>
 
       </View>
       
       <View style={styles.content}>
-
+      <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isEnabled ? "#4054e0" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => { toggleSwitch(); setNumber(Math.round(Math.random() * 1000)); setModalVisible(!modalVisible) }}
+            value={isEnabled}
+          />
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!isEnabled);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>{Number}</Text>
+                        <Pressable
+                            style={[styles.button2, styles.buttonClose]}
+                            onPress={() => setModalVisible(false)}
+                        >
+                            <Text style={styles.textStyle}>Hide Modal</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
       </View>
       
     </View>
   );
 }
 
-function Date({ navigation }) {
+function datePage({ navigation }) {
+
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+      const currentDate = selectedDate || date;
+      setShow(Platform.OS === 'ios');
+      setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+      setShow(true);
+      setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+      showMode('date');
+  };
+
+  const showTimepicker = () => {
+      showMode('time');
+  };
+
+  const showSelectedDate = () => {
+      const day = date.getDate();
+      const month = date.getMonth();
+      const year = date.getFullYear();
+      const hour = date.getHours();
+      const minute = date.getMinutes();
+      Alert.alert(
+          "Czas:  " + date.toLocaleTimeString() + "\nDzień: " + date.toDateString()
+      )
+  }
+
   return (
     <View>
       <View style={styles.navBar}>
@@ -174,29 +273,51 @@ function Date({ navigation }) {
         <Text> Select </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Switch" onPress={() => navigation.navigate('Switch')} >
-      <Text> Switch </Text>
+      <TouchableOpacity style={styles.button} title="SwitchPage" onPress={() => navigation.navigate('SwitchPage')} >
+      <Text> SwitchPage </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Date" onPress={() => navigation.navigate('Date')} >
-      <Text> Date </Text>
+      <TouchableOpacity style={styles.button} title="datePage" onPress={() => navigation.navigate('datePage')} >
+      <Text> datePage </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Modal" onPress={() => navigation.navigate('Modal')} >
-      <Text> Modal </Text>
+      <TouchableOpacity style={styles.button} title="ModalPage" onPress={() => navigation.navigate('ModalPage')} >
+      <Text> ModalPage </Text>
       </TouchableOpacity>
 
       </View>
       
-      <View style={styles.content}>
-
-      </View>
+       <View style={styles.content}>
+            <View style={styles.top}>
+                <View style={styles.topItem}><Button onPress={showDatepicker} title="Dzień" /></View>
+                <View style={styles.topItem}><Button onPress={showTimepicker} title="Godzina" /></View>  
+            </View>
+            <View style={styles.topItem}>
+                <Button onPress={showSelectedDate} title="Pokaż datę" />
+            </View>
+            {show && (
+                <DateTimePicker
+                    testID="dateTimePicker"
+                    value={date}
+                    mode={mode}
+                    is24Hour={true}
+                    display="default"
+                    onChange={onChange}
+                />
+            )}
+        </View>
       
     </View>
   );
 }
 
-function Modal({ navigation }) {
+function ModalPage({ navigation }) {
+  const [isEnabled, setIsEnabled] = React.useState(false);
+  const toggleSwitch = () => {
+      setIsEnabled(previousState => !previousState);
+      setModalVisible(!isEnabled);
+  };
+  const [modalVisible, setModalVisible] = React.useState(false);
   return (
     <View>
       <View style={styles.navBar}>
@@ -209,22 +330,54 @@ function Modal({ navigation }) {
         <Text> Select </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Switch" onPress={() => navigation.navigate('Switch')} >
-      <Text> Switch </Text>
+      <TouchableOpacity style={styles.button} title="SwitchPage" onPress={() => navigation.navigate('SwitchPage')} >
+      <Text> SwitchPage </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Date" onPress={() => navigation.navigate('Date')} >
-      <Text> Date </Text>
+      <TouchableOpacity style={styles.button} title="datePage" onPress={() => navigation.navigate('datePage')} >
+      <Text> datePage </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} title="Modal" onPress={() => navigation.navigate('Modal')} >
-      <Text> Modal </Text>
+      <TouchableOpacity style={styles.button} title="ModalPage" onPress={() => navigation.navigate('ModalPage')} >
+      <Text> ModalPage </Text>
       </TouchableOpacity>
 
       </View>
       
       <View style={styles.content}>
-
+      <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isEnabled ? "#4054e0" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => { toggleSwitch(); setNumber(Math.round(Math.random() * 1000)); setModalVisible(!modalVisible) }}
+            value={isEnabled}
+          />
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!isEnabled);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Zamknij modal aby pokazać ToastAndroid</Text>
+                        <Pressable
+                            style={[styles.button2, styles.buttonClose]}
+                            onPress={() => {
+                                setModalVisible(false);
+                                setTimeout(() => {
+                                    ToastAndroid.show("Toast Text", ToastAndroid.SHORT)
+                                }, 100);      
+                            }}
+                        >
+                            <Text style={styles.textStyle}>Hide Modal</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
       </View>
       
     </View>
@@ -248,19 +401,19 @@ export default function App() {
         options={{ title: 'Select' }}
       />
       <Stack.Screen 
-        name="Switch" 
-        component={Switch} 
-        options={{ title: 'Switch' }}
+        name="SwitchPage" 
+        component={SwitchPage} 
+        options={{ title: 'SwitchPage' }}
       />
       <Stack.Screen 
-        name="Date" 
-        component={Date} 
-        options={{ title: 'Date' }}
+        name="datePage" 
+        component={datePage} 
+        options={{ title: 'datePage' }}
       />
       <Stack.Screen 
-        name="Modal" 
-        component={Modal} 
-        options={{ title: 'Modal' }}
+        name="ModalPage" 
+        component={ModalPage} 
+        options={{ title: 'ModalPage' }}
       />
     </Stack.Navigator>
   </NavigationContainer>
@@ -279,6 +432,9 @@ const styles = StyleSheet.create({
         marginLeft: '20%',
         marginRight: '20%',
         height: '90%',
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
       }
     })
   },
@@ -377,5 +533,62 @@ text: {
 },
 scrollView: {
   marginHorizontal: 30,
+},
+modalView: {
+  margin: 20,
+  backgroundColor: "white",
+  borderRadius: 20,
+  padding: 35,
+  alignItems: "center",
+  shadowColor: "#000",
+  shadowOffset: {
+      width: 0,
+      height: 2
+  },
+  shadowOpacity: 0.25,
+  shadowRadius: 4,
+  elevation: 5
+},
+centeredView: {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  marginTop: 22
+},
+modalView: {
+  margin: 20,
+  backgroundColor: "white",
+  borderRadius: 20,
+  padding: 35,
+  alignItems: "center",
+  shadowColor: "#000",
+  shadowOffset: {
+      width: 0,
+      height: 2
+  },
+  shadowOpacity: 0.25,
+  shadowRadius: 4,
+  elevation: 5
+},
+modalText: {
+  marginBottom: 15,
+  textAlign: "center"
+},
+button2: {
+  borderRadius: 20,
+  padding: 10,
+  elevation: 2
+},
+buttonOpen: {
+  backgroundColor: "#F194FF",
+},
+top: {
+  display: 'flex',
+  flexDirection: 'row',
+  alignContent: 'space-between'
+},
+topItem: {
+  margin: 10,
+  width: 100
 }
 });
