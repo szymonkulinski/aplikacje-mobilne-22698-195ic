efa3d0f5a1f60644fa1048fab178aa2c482c5eb9
szymonkulinski <56955430+szymonkulinski@users.noreply.github.com>
2022-05-24 20:13:20 +0200

    Update

--- Lab5/layout-navigation/App.js
+++ Lab5/layout-navigation/App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{ useState, Suspense } from 'react';
+import React,{ useState, Suspense, } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {ActivityIndicator, StyleSheet, Text, View, TouchableOpacity, Platform, SafeAreaView, ScrollView } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View, TouchableOpacity, Platform, SafeAreaView, ScrollView, TouchableHighlight, } from 'react-native';
+import { WebView } from 'react-native-webview';
+import Swipeable from 'react-native-swipeable';
 
 function ScrollBar1({ navigation }) {
   return (
@@ -85,7 +87,6 @@ function ScrollBar1({ navigation }) {
 
 
 function ScrollBar2({ navigation }) {
-
   return (
     <View>
       <View style={styles.navBar}>
@@ -107,16 +108,88 @@ function ScrollBar2({ navigation }) {
       </TouchableOpacity>
       
       </View>
-      <View style={styles.content}>
+          <ScrollView style={styles.scrollView2}>
+            <View style={styles.box1}>
+              <Text style={styles.textBox}>
+                1
+              </Text>
+            </View>
 
- 
-      </View>
+            <View style={styles.box2}>
+              <Text style={styles.textBox}>
+                2
+              </Text>
+            </View>
+            
+            <View style={styles.box1}>
+              <Text style={styles.textBox}>
+                1
+              </Text>
+            </View>
+
+            <View style={styles.box2}>
+              <Text style={styles.textBox}>
+                2
+              </Text>
+            </View>
+
+            <View style={styles.box1}>
+              <Text style={styles.textBox}>
+                1
+              </Text>
+            </View>
+
+            <View style={styles.box2}>
+              <Text style={styles.textBox}>
+                2
+              </Text>
+            </View>
+
+            <View style={styles.box1}>
+              <Text style={styles.textBox}>
+                1
+              </Text>
+            </View>
+
+            <View style={styles.box2}>
+              <Text style={styles.textBox}>
+                2
+              </Text>
+            </View>
+
+            <View style={styles.box1}>
+              <Text style={styles.textBox}>
+                1
+              </Text>
+            </View>
+
+            <View style={styles.box2}>
+              <Text style={styles.textBox}>
+                2
+              </Text>
+            </View>
+
+            <View style={styles.box1}>
+              <Text style={styles.textBox}>
+                1
+              </Text>
+            </View>
+
+            <View style={styles.box2}>
+              <Text style={styles.textBox}>
+                2
+              </Text>
+            </View>
+
+          </ScrollView>
       
     </View>
   );
 }
 
 function Touchable({ navigation }) {
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(count + 1);
   return (
     <View>
       <View style={styles.navBar}>
@@ -138,16 +211,80 @@ function Touchable({ navigation }) {
       </TouchableOpacity>
 
       </View>
-      
-      <View style={styles.content}>
 
+      <View style={styles.touchcontent}>
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.touchButton}>
+            <Text>Touch Here</Text>
+          </View>
+        </TouchableHighlight>
+        <View style={styles.countContainer}>
+          <Text style={styles.countText}>{count ? count : null}</Text>
+        </View>
       </View>
-      
+
     </View>
   );
 }
 
 function Swipe({ navigation }) {
+  const leftContent=[
+    <View style={[styles.leftSwipeItem, {backgroundColor: 'lightskyblue'}]}>
+      <Text>Pull action</Text>
+    </View>
+  ];
+
+  const rightButtons = [
+    <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: 'lightseagreen'}]}>
+    <Text>Green</Text>
+  </TouchableOpacity>,
+  <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: 'orchid'}]}>
+    <Text>Orchid</Text>
+  </TouchableOpacity>
+  ];
+
+  const leftContent2=[
+    <View style={[styles.leftSwipeItem, {backgroundColor: 'lightskyblue'}]}>
+      <Text>Pull action</Text>
+    </View>
+  ];
+
+  const rightButtons2 = [
+    <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: 'lightseagreen'}]}>
+    <Text>1</Text>
+  </TouchableOpacity>,
+  <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: 'orchid'}]}>
+    <Text>2</Text>
+  </TouchableOpacity>,
+  <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: 'lightseagreen'}]}>
+    <Text>3</Text>
+  </TouchableOpacity>,
+  <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: 'orchid'}]}>
+    <Text>4</Text>
+  </TouchableOpacity>
+  ];
+
+  const leftContent3=[
+    <View style={[styles.leftSwipeItem, {backgroundColor: 'lightskyblue'}]}>
+      <Text>Pull action</Text>
+    </View>
+  ];
+
+  const rightButtons3 = [
+    <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: '#aaaaaa'}]}>
+    <Text>1</Text>
+  </TouchableOpacity>,
+  <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: '#bbbbbb'}]}>
+    <Text>2</Text>
+  </TouchableOpacity>,
+  <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: '#cccccc'}]}>
+    <Text>1</Text>
+  </TouchableOpacity>,
+  <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: '#dddddd'}]}>
+    <Text>2</Text>
+  </TouchableOpacity>
+  ];
+  
   return (
     <View>
       <View style={styles.navBar}>
@@ -171,7 +308,15 @@ function Swipe({ navigation }) {
       </View>
       
       <View style={styles.content}>
-
+        <Swipeable leftContent={leftContent} rightButtons={rightButtons}>
+          <Text style={styles.textBox}>Swipe1</Text>
+        </Swipeable>
+        <Swipeable leftContent={leftContent2} rightButtons={rightButtons2}>
+          <Text style={styles.textBox}>Swipe2</Text>
+        </Swipeable>
+        <Swipeable leftContent={leftContent3} rightButtons={rightButtons3}>
+          <Text style={styles.textBox}>Swipe3</Text>
+        </Swipeable>
       </View>
       
     </View>
@@ -309,6 +454,45 @@ const styles = StyleSheet.create({
 
   scrollView: {
     //backgroundColor: 'brown',
-    marginHorizontal: 5,
+    marginHorizontal: 50,
+  },
+
+  scrollView2: {
+    flexGrow: 1, 
+    borderColor: '#FFFFE0', 
+    borderWidth: 5
+  },
+  box1:{
+    flex:1,
+    backgroundColor: '#F0E68C',
+  },
+  box2:{
+    flex:1,
+  },
+  textBox:{
+    fontSize: 40,
+  },
+  touchContent: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  touchButton: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  countContainer: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  countText: {
+    color: '#FF00FF',
+  },
+  leftSwipeItem: {
+    height:50,
+  },
+  rightSwipeItem: {
+    height:50,
   },
 });
